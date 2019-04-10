fb2d9e470bb8d4ad60654a459fbe714019bccc47
Dragonite <haolin97@gmail.com>
2019-04-10 17:05:48 +0800

    Added homepage, base.html and sidebar and header files.

--- app/views.py
+++ app/views.py
@@ -1,6 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Haolin'}  # fake user
+    posts = [  # fake array of posts
+        { 
+            'author': {'nickname': 'Haolin'}, 
+            'body': 'I caught a herring!' 
+        },
+        { 
+            'author': {'nickname': 'Luna'}, 
+            'body': 'I caught a snapper!' 
+        }
+    ]
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
