675541c0b513ee4139ad0a95f6c92c8af516ec07
Dragonite <haolin97@gmail.com>
2019-04-17 17:34:04 +0800

    Reformat all files

--- app/routes.py
+++ app/routes.py
@@ -1,49 +1,57 @@
 from flask import render_template, flash, redirect, url_for, Markup
+
 from app import app
 from app.forms import LoginForm
 
 # Mimic Admin User
-user = {'nickname': 'Haolin', 'admin': True, 'id': 1}
+# user = {'nickname': 'Haolin', 'admin': True, 'id': 1}
 
 # Mimic Logged In User
-#user = {'nickname': 'Leon', 'admin': False, 'id': 2}
+# user = {'nickname': 'Leon', 'admin': False, 'id': 2}
 
 # Mimic Default User
-# user={}
+user = {}
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     posts = [  # fake array of posts
-        { 
-            'author': {'nickname': 'Haolin'}, 
-            'body': 'I caught a herring!' 
+        {
+            'author': {'nickname': 'Haolin'},
+            'body': 'I caught a herring!'
         },
-        { 
-            'author': {'nickname': 'Luna'}, 
-            'body': 'I caught a snapper!' 
+        {
+            'author': {'nickname': 'Luna'},
+            'body': 'I caught a snapper!'
         }
     ]
     return render_template("index.html", title='Home', user=user, posts=posts)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        message=Markup('<script>Notify("Login requested for user {}, Remember me = {}", null, null, "danger")</script>'.format(form.username.data, form.remember_me.data))
+        message = Markup(
+            '<script>Notify("Login requested for user {}, Remember me = {}", null, null, "danger")</script>'.format(
+                form.username.data, form.remember_me.data))
         flash(message)
         # flash('Login requested for user {}, remember_me={}'.format(
         #     form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form, user=user)
 
+
 @app.route('/help')
 def help():
     return render_template("help.html", title='Help', user=user)
 
+
 @app.route('/current')
 def current():
     return render_template("current.html", title='Current Polls', user=user)
+
 
 @app.route('/completed')
 def completed():
@@ -54,12 +62,12 @@ def completed():
 def create():
     return render_template("create.html", title='Create A Poll', user=user)
 
+
 @app.route('/users')
 def users():
     return render_template("users.html", title='Users', user=user)
 
+
 @app.route('/profile')
 def profile():
     return render_template("profile.html", title='My Profile', user=user)
-
-                 
