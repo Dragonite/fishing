c0af2b6d47aef3ff1e86527234bc08852ba7134d
Dragonite <haolin97@gmail.com>
2019-04-19 23:24:16 +0800

    Fix bug where password was never checked

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template, flash, redirect, url_for, Markup, request
+from flask import render_template, flash, redirect, url_for, Markup
+from flask_login import login_required, current_user, login_user, logout_user
+
 from app import app, db
 from app.forms import LoginForm, CreatePollForm
-from flask_login import login_required, current_user, login_user, logout_user
 from app.models import User
 
 
@@ -14,7 +15,7 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-         return redirect(url_for('index'))
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -22,22 +23,19 @@ def login():
         #     flash('Invalid username or password')
         #     return redirect(url_for('login'))
         # login_user(user, remember=form.remember_me.data)
-       # next_page = request.args.get('next')   #########
-       # if not next_page or url_parse(next_page).netloc != '':
-       #      next_page = url_for('index')
-        #return redirect(url_for('next_page')) 
+        # next_page = request.args.get('next')   #########
+        # if not next_page or url_parse(next_page).netloc != '':
+        #      next_page = url_for('index')
+        # return redirect(url_for('next_page'))
 
-        if user:
-            login_user(user, remember=form.remember_me.data)
-            #session['was_once_logged_in'] = True
-            return redirect(url_for('index'))
-        else:
+        if user is None or not user.check_password(form.password.data):
             flash(Markup('<script>Notify("Invalid username or password.", null, null, "danger")</script>'))
             return redirect(url_for('login'))
-
+            # session['was_once_logged_in'] = True
+        login_user(user, remember=form.remember_me.data)
+        flash(Markup('<script>Notify("You have successfully logged in.", null, null, "success")</script>'))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
-
-
 
 
 @app.route('/create', methods=['GET', 'POST'])
@@ -85,14 +83,13 @@ def users():
 def profile():
     return render_template("profile.html", title='My Profile')
 
+
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     flash(Markup('<script>Notify("You have successfully logged yourself out.", null, null, "success")</script>'))
-    return redirect(url_for('index'))        
-
-
+    return redirect(url_for('index'))
 
 
 @app.route('/register', methods=['GET', 'POST'])
