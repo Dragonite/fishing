6d0cdf0ca4b4a92b7aa7656518c2c54add1d2b81
Luna <22187554@student.edu.au>
2019-04-20 19:05:09 +0800

    Redefine Poll object, add methods: addCandidate, howManyCandidates, howManyResponses

--- app/controllers.py
+++ app/controllers.py
@@ -1,1 +1,6 @@
 from app.models import User, Poll, Candidates, Response
+from app import app, db
+
+
+def createPoll(User, Poll, Candidates):
+    return Poll
--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from datetime import timedelta  
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
@@ -52,8 +53,13 @@ class User(UserMixin, db.Model):
     def check_password(self, pwd):
         return check_password_hash(self.pwdHash, pwd)
 
-    def is_committed(self):
-        return self.project_id is not None
+    def delete_user(self)
+        if self.isAdmin:
+            raise ValueError('You cannot delete Admin user!!')
+        else: 
+            self.lastModifiedAt=datetime.utcnow()
+            self.isActive=False
+
 
 
 
@@ -72,35 +78,103 @@ class Poll(db.Model):
     completedAt=db.Column(db.DateTime)
 
     orderCandidatesBy=db.Column(Enum('Desc','Acs','SpecialOrder'))
-   
     minResponses=db.Column(db.Integer)
-    
-    createdBy=db.Column(db.Integer, db.ForeignKey('users.userId'), nullable=False)
+    openAt=db.Column(db.DateTime)
+    closeAt=db.Column(db.DateTime)
+
+    createdByUserId=db.Column(db.Integer, db.ForeignKey('users.userId'), nullable=False)
 
     isOpenPoll=db.Column(db.Boolean) 
     isActive=db.Column(db.Boolean) 
 
-class Candidate(db.Model):
-    
-    __tablename__ = 'candidates'
-    __table_args__ = {'sqlite_autoincrement': True}
+    class Candidate(db.Model):
+        
+        __tablename__ = 'candidates'
+        __table_args__ = {'sqlite_autoincrement': True}
 
-    candidateId=db.Column(db.Integer, primary_key=True, autoincrement=True)
-    candidateDescription=db.Column(db.String(128), nullable=False)
-    displayOrder=db.Column(db.Integer)
-    isActive=db.Column(db.Boolean) 
-    pollId=db.Column(db.Integer, db.ForeignKey('polls.pollId'), nullable=False)
+        candidateId=db.Column(db.Integer, primary_key=True, autoincrement=True)
+        candidateDescription=db.Column(db.String(128), nullable=False)
+        displayOrder=db.Column(db.Integer)
+        isActive=db.Column(db.Boolean) 
+        pollId=db.Column(db.Integer, db.ForeignKey('polls.pollId'), nullable=False)
+        
+       
+    class Response(db.Model):
 
-class Response(db.Model):
+        __tablename__ = 'responses'
+        __table_args__ = (db.UniqueConstraint('userId', 'pollId', 'candidateId', 'response', name='unique_resonses'),{'sqlite_autoincrement': True})
 
-    __tablename__ = 'responses'
-    __table_args__ = (db.UniqueConstraint('userId', 'pollId', 'candidateId', 'response', name='unique_resonses'),{'sqlite_autoincrement': True})
+        responseId=db.Column(db.Integer, primary_key=True, autoincrement=True)
+        userId=db.Column(db.Integer, db.ForeignKey('users.userId'), nullable=False)
+        pollId=db.Column(db.Integer, db.ForeignKey('polls.pollId'), nullable=False)
+        candidateId=db.Column(db.Integer, db.ForeignKey('candidates.candidateId'), nullable=False)
+        
+        response=db.Column(db.Integer)
+        createdAt=db.Column(db.DateTime)
 
-    responseId=db.Column(db.Integer, primary_key=True, autoincrement=True)
-    userId=db.Column(db.Integer, db.ForeignKey('users.userId'), nullable=False)
-    pollId=db.Column(db.Integer, db.ForeignKey('polls.pollId'), nullable=False)
-    candidateId=db.Column(db.Integer, db.ForeignKey('candidates.candidateId'), nullable=False)
-    
-    response=db.Column(db.Integer)
-    createdAt=db.Column(db.DateTime)
+    def __init__(self, title, description, minResponses, orderCandidatesBy, isOpenPoll, openAt, closeAt, User):
+        self.Candidate=self.Candidate()
+        self.Candidate=[]
+
+        self.Response=self.Response()
+        self.Response=[]
+
+        self.lastModifiedAt=None
+        self.completedAt=None
+        self.title=title
+        self.description=description
+        
+        if minResponses>0:
+            self.minResponses=minResponses
+        else:
+            self.minResponses=-1
+        
+        if orderCandidatesBy==None:
+            self.orderCandidatesBy='Acs'
+        else:
+            self.orderCandidatesBy=orderCandidatesBy
+
+        if isOpenPoll==None:
+            self.isOpenPoll=False
+        else:
+            self.isOpenPoll=isOpenPoll
+        
+        if openAt==None:
+            self.openAt=datetime.utcnow()
+        else:
+            self.openAt=openAt
+        
+        if closeAt==None:
+            self.closeAt=datetime.utcnow()+timedelta(days=7)
+        else:
+            self.closeAt=closeAt
+               
+        self.createdByUserId=User.userId
+        self.createdAt=datetime.utcnow()
+        self.isActive=1
+
+    def addCandidate(self, candidateDescription, displayOrder):
+        if candidateDescription==None:
+             raise ValueError('You must enter the candidate details!')
+        else:
+            if self.orderCandidatesBy=='SpecialOrder' and displayOrder==None:
+                raise ValueError('You must enter the order you want to display this candidate!')
+            else:
+                candidate = Poll.Candidate()
+                candidate.candidateDescription=candidateDescription
+                candidate.displayOrder=displayOrder
+                candidate.pollId=self.pollId
+                self.Candidate.append(candidate)
+
+    def howManyCandidates(self):
+        return len(self.Candidate)
+
+    def howManyResponses(self):
+        return len(self.Response)
+
+
+
+
+
+           
 
