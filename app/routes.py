d388e7fd3610b2c6230d077ec14bbb3e0efa2a55
Luna <22187554@student.edu.au>
2019-04-20 15:35:05 +0800

    edit register()

--- app/routes.py
+++ app/routes.py
@@ -102,7 +102,7 @@ def register():
         return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
+        user = User(username=form.username.data, email=form.email.data, firstName=form.firstName.data, lastName=form.lastName.data, ad_street=form.ad_street.data, ad_suburb=form.ad_suburb.data, ad_state=form.ad_state.data, createdAt=datetime.utcnow(), isActive=1, isAdmin=0)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
