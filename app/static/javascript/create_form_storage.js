2fde257ee7a0c4d17815aa0caba9940cca705fa3
Dragonite <haolin97@gmail.com>
2019-05-08 16:06:30 +0800

    Refractor create_form_storage.js

--- app/static/javascript/create_form_storage.js
+++ app/static/javascript/create_form_storage.js
@@ -1,22 +1,26 @@
-let testObject = localStorage.createForm ? localStorage.createForm : JSON.stringify({title: "", description: "", choices: []});
-    localStorage.setItem('createForm', testObject);
-    $(document).ready(function () {
-      $('#title').val(JSON.parse(localStorage.createForm).title);
-      $('#description').val(JSON.parse(localStorage.createForm).description);
-    });
-    document.getElementById('save-button').addEventListener('click', handleSave);
-    document.getElementById('clear-button').addEventListener('click', handleClear);
+let testObject = localStorage.createForm ? localStorage.createForm : JSON.stringify({
+  title: "",
+  description: "",
+  choices: []
+});
+localStorage.setItem('createForm', testObject);
+$(document).ready(function () {
+  $('#title').val(JSON.parse(localStorage.createForm).title);
+  $('#description').val(JSON.parse(localStorage.createForm).description);
+});
+document.getElementById('save-button').addEventListener('click', handleSave);
+document.getElementById('clear-button').addEventListener('click', handleClear);
 
-    function handleSave() {
-      let createFormAttributes = JSON.parse(localStorage.createForm);
-      createFormAttributes.title = document.getElementById('title').value;
-      createFormAttributes.description = document.getElementById('description').value;
-      localStorage.setItem('createForm', JSON.stringify(createFormAttributes));
-      Notify("Your current fields have been saved.", null, null, 'success');
-    }
+function handleSave() {
+  let createFormAttributes = JSON.parse(localStorage.createForm);
+  createFormAttributes.title = document.getElementById('title').value;
+  createFormAttributes.description = document.getElementById('description').value;
+  localStorage.setItem('createForm', JSON.stringify(createFormAttributes));
+  Notify("Your current fields have been saved.", null, null, 'success');
+}
 
-    function handleClear() {
-      localStorage.setItem('createForm', JSON.stringify({title: "", description: "", choices: []}));
-      location.reload();
-      Notify("Your fields have been reset.", null, null, 'success');
-    }
+function handleClear() {
+  localStorage.setItem('createForm', JSON.stringify({title: "", description: "", choices: []}));
+  location.reload();
+  Notify("Your fields have been reset.", null, null, 'success');
+}
