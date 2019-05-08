ec26ed78aa6d60cf9067fa021d506468a501eca1
Luna <22187554@student.edu.au>
2019-05-08 21:26:50 +0800

    implement API

--- app/api/users.py
+++ app/api/users.py
@@ -2,7 +2,7 @@
 from app import db
 from app.models import User
 # from app.api.errors import bad_request, error_response
-from flask import jsonify, url_for, request#, g, abort
+from flask import jsonify, url_for, request, g, abort
 # from app.api.auth import token_auth
 from app.api import bp
 
@@ -11,15 +11,18 @@ from app.api import bp
 @bp.route('/users/<int:userId>', methods=['GET'])
 def get_user(userId):
     # if g.current_user != userId:
-    #     abor(403)
+    #      abort(403)
     user=User.query.get_or_404(userId).to_dict()
     return jsonify(user)
-    # return jsonify(User.query.get_or_404(userId).to_dict())
 
 
-# @app.route('/users', methods=['GET'])
-# def get_users():
-#     pass
+
+@bp.route('/users', methods=['GET'])
+def get_users():
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    data = User.to_collection_dict(User.query, page, per_page, 'api.get_users')
+    return jsonify(data)
 
 # @app.route('/users', methods=['POST'])
 # def create_user():
