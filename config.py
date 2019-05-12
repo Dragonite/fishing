bab12ddc23e1ed0ffa47b4ace7b63c42a868e856
Luna <22187554@student.edu.au>
2019-05-12 20:31:02 +0800

    test

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
 import os
-basedir=os.path.abspath(os.path.dirname(__file__))
+from dotenv import load_dotenv
+
+
+# basedir=os.path.abspath(os.path.dirname(__file__))
 DATABASE_URL=''
 dbName='app.db'
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     '''SQLALCHEMY_DATABASE = os.environ.get(DATABASE_URL) or 'sqlite:///' + os.path.join(basedir, dbName)'''
