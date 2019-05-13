fdbab999036158ed83e75704991b0748e9b54efb
Dragonite <haolin97@gmail.com>
2019-05-13 23:51:43 +0800

    Add form, fix error

--- app/main/routes.py
+++ app/main/routes.py
@@ -51,7 +51,7 @@ def create():
             else:  
                 if createPoll(poll)==True:
                     flash('Poll has been created successfully!')
-                    return render_template("currentPollView.html", title=poll.title, poll=poll)
+                    return render_template("currentPollView.html", title=poll.title, poll=poll, form=form)
 
                 else:
                     flash('something is wrong!')
