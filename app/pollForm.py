3658ebfca048f66b151a178ee388a9a71dd63273
Dragonite <haolin97@gmail.com>
2019-05-13 16:46:39 +0800

    Revert "fix current_user isAdmin issue"
    This reverts commit 0a400ec02bf92fe1b18be2e41375040a2254cb66.

--- app/pollForm.py
+++ app/pollForm.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, FieldList, IntegerField
 from wtforms.validators import  ValidationError, DataRequired, Email, EqualTo, NumberRange
 from wtforms.fields.html5 import EmailField
-from app.models import User, Poll
+from app.models import User
 # from flask_babel import _, lazy_gettext as _l
 
 
@@ -22,9 +22,9 @@ class CreatePollForm(FlaskForm):
 
 
 class CreateResponseForm(FlaskForm):
-    
-    def __init__(self, pollId):
-        poll=Poll.query.filter_by(pollId=pollId).first() 
+    # poll=Poll()
+    def __init__(self, poll):
+        self.poll=poll
 
 
     pollId=IntegerField()
@@ -32,4 +32,4 @@ class CreateResponseForm(FlaskForm):
     candidateId= FieldList(IntegerField())
     response= FieldList(IntegerField())
     submit = SubmitField('Submit my response')
-    # responses = FieldList('Responses', choices=[(c.candidateId, c.candidateDescription) for c in self.poll.Candidate])
+    # phonetype = SelectField(gettext("Type"), choices=[(c.candidateId, c.candidateDescription) for c in poll.Candidate]
