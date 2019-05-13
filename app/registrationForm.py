40a2ab0a9c83e7beebeeeeae58984b3b5e8b4436
Dragonite <haolin97@gmail.com>
2019-05-13 10:15:30 +0800

    Finish register form

--- app/registrationForm.py
+++ app/registrationForm.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextA
 from wtforms.validators import  ValidationError, DataRequired, Email, EqualTo, NumberRange
 from wtforms.fields.html5 import EmailField
 from app.models import User
-from flask_babel import _, lazy_gettext as _l
+from flask_babel import lazy_gettext as _l
 
 
 class RegistrationForm(FlaskForm):
@@ -12,17 +12,17 @@ class RegistrationForm(FlaskForm):
     firstName = StringField('First Name', validators=[DataRequired()])
     lastName= StringField('Last Name')
 
-    email = EmailField('Email address', validators=[DataRequired(), Email()])
+    email = EmailField('Email Address', validators=[DataRequired(), Email()])
     # password2 =PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    password2 = PasswordField(_l('Repeat Password'), validators=[DataRequired(),  EqualTo('password')])
+    password2 = PasswordField('Confirm Password', validators=[DataRequired(),  EqualTo('password')])
     
     ad_street=StringField('Street Address')
     ad_suburb=StringField('City/Suburb')
-    ad_state=SelectField('State', choices=[('WA','Western Australia'), ('QLD','Queensland'), ('SA','South Australia'),('ACT','Australian Capital Territory'),('NT','Northern Territory'), ('TAS','Tasmania')])
-    ad_country=SelectField('Country', choices=[('Aus','Western Australia')])
+    ad_state=SelectField('State', choices=[('WA','Western Australia'), ('QLD','Queensland'), ('SA','South Australia'),('ACT','Australian Capital Territory'),('NT','Northern Territory'), ('TAS','Tasmania')], default='WA')
+    ad_country=SelectField('Country', choices=[('Aus','Australia')])
 
     
-    submit = SubmitField('Sign up')
+    submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
         user = User.query.filter_by(username= username.data).first()
