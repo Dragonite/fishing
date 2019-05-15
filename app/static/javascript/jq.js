13450cfb6504e3263eccce0d2adc919b93eaba5a
Dragonite <haolin97@gmail.com>
2019-05-15 13:38:10 +0800

    Add table to userArchive

--- app/static/javascript/jq.js
+++ app/static/javascript/jq.js
@@ -29,7 +29,7 @@ $(document).ready(function () {
 
   $('#table-active, #table-inactive').DataTable();
 
-  $('#table-polls, #table-responses').DataTable( {
+  $('#table-polls, #table-responses, #table-users').DataTable( {
     "aLengthMenu": [[5, 10, 15, -1], [5, 10, 15, "All"]],
     "pageLength": 5
     } );
